2bfc70c0815e92c10c6d4df7b70a4da7d4090616
Ammar Mirza <itsammarmirza@gmail.com>
2024-06-21 15:22:45 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -31086,10 +31086,9 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput("time", nameToGreet);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 const core = require('@actions/core');
-const matter = require('gray-matter');
 const github = require('@actions/github');
 
-export async function run() {
-   console.log("test")
-   try {
-    const host = core.getInput("host");
-    const file = core.getInput("file");
-    const hashnode_key = core.getInput("hashnode_key")
-
-    console.log({
-      host,
-      file,
-      hashnode_key
-    })
-   //  core.setSecret(hashnode_key)
-   //  const content = await fs.readFile(file, "utf8");
-   //  const parsedArticle = matter(content, { language: "yaml" });
-
-    core.setOutput("result_json", 'hsskaska')
-    core.setOutput("host__name", host)
-    core.setOutput("secret__key", hashnode_key)
-    core.setOutput("file__name", file)
-    
-   } catch (error) {
-      core.setFailed(error)
-   }
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", nameToGreet);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run()
