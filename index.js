52cef1a3e207e44253170d8047df6b8b04b5adee
Ammar Mirza <itsammarmirza@gmail.com>
2024-06-21 15:04:09 +0530

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const matter = require('gray-matter');
+const github = require('@actions/github');
+
+export async function run() {
+   try {
+    const host = core.getInput("host");
+    const file = core.getInput("file");
+    const hashnode_key = core.getInput("hashnode_key")
+
+   //  core.setSecret(hashnode_key)
+   //  const content = await fs.readFile(file, "utf8");
+   //  const parsedArticle = matter(content, { language: "yaml" });
+
+    core.setOutput("result_json", 'hsskaska')
+    core.setOutput("host__name", host)
+    core.setOutput("secret__key", hashnode_key)
+    core.setOutput("file__name", file)
+    
+   } catch (error) {
+      core.setFailed(error)
+   }
+}
+
+run()
