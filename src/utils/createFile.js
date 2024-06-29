561168e2a9ff84bb28fbeff5f5f7cc98626571ca
Ammar Mirza <itsammarmirza@gmail.com>
2024-06-29 15:25:34 +0530

    Test

--- src/utils/createFile.js
+++ src/utils/createFile.js
@@ -4,7 +4,7 @@ const { Base64 } = require("js-base64")
 const github = require("@actions/github");
 
 const octokit = new Octokit({
-  auth: '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',
+  auth: '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',
 });
 
 export const createFile = async () => {
