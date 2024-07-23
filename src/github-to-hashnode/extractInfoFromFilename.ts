b4a98dabc46e0672274596eaf7403ab6cdbc0bd3
Ammar Mirza <itsammarmirza@gmail.com>
2024-07-23 20:50:39 +0530

    Added Delete Article in Hashnode to GitHub route

--- src/github-to-hashnode/extractInfoFromFilename.ts
+++ src/github-to-hashnode/extractInfoFromFilename.ts
@@ -1,4 +1,9 @@
-export const extractInfoFromFilename = (file: string) => {
+type ExtractedInfo = {
+    postId: string
+    slug: string | undefined
+}
+
+export const extractInfoFromFilename = (file: string): ExtractedInfo => {
     const postId = file.split('-')[0]
     const slug = file.split('-').slice(1).join('-')
 
